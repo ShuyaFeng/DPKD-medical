8a1981310669bdeac9d695b024b7013113280f68
Armaghan Mubeen <ab36@uab.edu>
2026-07-17 03:47:15 -0500

    Fix Slurm paths: ialam -> ab36 in all three dataset rerun scripts

--- phase1_distillation/slurm/run_busi_canal_k3_eps2.sh
+++ phase1_distillation/slurm/run_busi_canal_k3_eps2.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
-#SBATCH --output=/home/ialam/DPKD-medical/phase1_distillation/slurm_logs/busi_canal_k3_epssweep_%j.out
-#SBATCH --error=/home/ialam/DPKD-medical/phase1_distillation/slurm_logs/busi_canal_k3_epssweep_%j.err
+#SBATCH --output=/home/ab36/DPKD-medical/phase1_distillation/slurm_logs/busi_canal_k3_epssweep_%j.out
+#SBATCH --error=/home/ab36/DPKD-medical/phase1_distillation/slurm_logs/busi_canal_k3_epssweep_%j.err
 
 set -e
 
@@ -26,7 +26,8 @@ if [[ "$CONDA_DEFAULT_ENV" != "mmseg-cu124-240" ]]; then
     exit 1
 fi
 
-cd /home/ialam/DPKD-medical/phase1_distillation
+mkdir -p /home/ab36/DPKD-medical/phase1_distillation/slurm_logs
+cd /home/ab36/DPKD-medical/phase1_distillation
 
 python -u busi_canal_k3_eps2.py --seeds 5 --te 60 --se 40 --epsilons "0.1,0.5,1,2,4,6,8"
 
